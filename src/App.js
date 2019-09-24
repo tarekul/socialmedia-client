766d63aadf190b7afa1b82d374fa797586410a1d
Tarek <bayridge4th@gmail.com>
2019-09-23 20:27:48 -0400

    home,login,signup page created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+class App extends Component {
+  render(){
+    return (
+      <div className='App'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path = "/" component={home} />
+            <Route path = "/login" component={login} />
+            <Route path = "/signup" component={signup} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
