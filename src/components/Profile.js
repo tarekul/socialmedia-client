552f0e4b25552a327e1b823979d77848eba40f7b
Pursuit <pursuit@Pursuits-MacBook-Air.local>
2019-11-23 05:32:23 -0500

    image upload button created and image is uploaded to bucket and user image on firebase

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,14 +5,19 @@ import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 //MUI stuff
 import Button from "@material-ui/core/Button";
-import { Paper, Typography } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import MuiLink from "@material-ui/core/Link";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 // Icons
 import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import EditIcon from "@material-ui/icons/Edit";
 //Redux
 import { connect } from "react-redux";
+import { logoutUser, uploadImage } from "../redux/actions/userActions";
 
 const styles = {
   paper: {
@@ -47,11 +52,6 @@ const styles = {
     "& hr": {
       border: "none",
       margin: "0 0 10px 0"
-    },
-    "& svg.button": {
-      "&:hover": {
-        cursor: "pointer"
-      }
     }
   },
   buttons: {
@@ -63,6 +63,18 @@ const styles = {
 };
 
 class Profile extends Component {
+  handleImageChange = e => {
+    const image = e.target.files[0];
+    // send to server
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    this.props.uploadImage(formData);
+  };
+
+  handleEditPicture = () => {
+    const fileInput = document.getElementById("imageInput");
+    fileInput.click();
+  };
   render() {
     const {
       classes,
@@ -79,6 +91,17 @@ class Profile extends Component {
           <div className={classes.profile}>
             <div className="image-wrapper">
               <img src={imageUrl} alt="profile" className="profile-image" />
+              <input
+                type="file"
+                id="imageInput"
+                hidden="hidden"
+                onChange={this.handleImageChange}
+              />
+              <Tooltip title="Edit Profile Picture" placement="top">
+                <IconButton onClick={this.handleEditPicture} className="button">
+                  <EditIcon color="primary" />
+                </IconButton>
+              </Tooltip>
             </div>
             <hr />
             <div className="profile-details">
@@ -155,8 +178,15 @@ const mapStateToProps = state => ({
   user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired
 };
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Profile));
